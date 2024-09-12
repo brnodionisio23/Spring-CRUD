bd903fd31aa1230734ced64ba507b813009eebfa
Breno Dionisio <brnodionisio23@gmail.com>
2024-09-12 15:26:24 -0300

    Criando rotas basicas de category

--- src/main/java/dev/breno/to_do/list/Controller/CategoryController.java
+++ src/main/java/dev/breno/to_do/list/Controller/CategoryController.java
@@ -10,22 +10,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "categoria")
 public class CategoryController {
 
     @Autowired
     private CategoryService service;
 
-    @PostMapping("/add")
-    public ResponseEntity<Category> insert(@RequestBody Category obj) {
-        Category newObj = service.insert(obj);
-        return new ResponseEntity<>(newObj, HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<Category> save(@RequestBody Category obj) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(obj));
     }
 
-    @GetMapping("/findAll")
+    @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable(name = "id") Long id, @RequestBody Category obj) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(id, obj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable(name = "id") Long id) {
+        service.delete(id);
     }
 
 }
